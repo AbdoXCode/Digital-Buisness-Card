3c4da76e2d4d033a749bdc9a67e770d074498aaf
Abdelrhman <adodoadodo2025@gmail.com>
2025-09-08 21:21:48 +0300

    Fix image source path in Info component

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,7 +2,10 @@ export default function Info() {
   return (
     <div>
       <div className="image-container">
-        <img src="./src/images/design-img.png" alt="girl image" />
+        <img
+          src="../Digital-Buisness-Card/src/images/design-img.png"
+          alt="girl image"
+        />
       </div>
       <h2>Laura Smith</h2>
       <p>Frontend Developer</p>
