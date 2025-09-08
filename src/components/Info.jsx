4c402afb81dd519a8d8eb644f30a0ea7a6695933
Abdelrhman <adodoadodo2025@gmail.com>
2025-09-08 21:59:06 +0300

    Refactor code structure and remove redundant functions for improved readability and maintainability

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,14 +1,19 @@
-import DesignImg from "../images/design-img.png";
-
 export default function Info() {
   return (
     <div className="info">
       <div className="image-container">
-        <img src={DesignImg} alt="girl image" />
+        <img
+          src="/Digital-Buisness-Card/images/design-img.png"
+          alt="girl image"
+        />
       </div>
       <h2>Laura Smith</h2>
       <p>Frontend Developer</p>
       <a href="#">laurasmith.website</a>
+      <div className="buttons">
+        <button className="email">Email</button>
+        <button className="linkedIn">LinkedIn</button>
+      </div>
     </div>
   );
 }
