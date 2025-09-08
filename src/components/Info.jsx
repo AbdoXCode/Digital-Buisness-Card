1e54bb2360f05e068960002a6f5665ba319470dd
Abdelrhman <adodoadodo2025@gmail.com>
2025-09-08 20:44:16 +0300

    Initialize React application with Vite, add main entry point, global styles, and basic layout structure.

---
+++ src/components/Info.jsx
@@ -0,0 +1,11 @@
+export default function Info() {
+  return (
+    <div>
+      <div className="image-container">
+        <img src="./src/images/design-img.png" alt="girl image" />
+      </div>
+      <h2>Laura Smith</h2>
+      <p>Frontend Developer</p>
+    </div>
+  );
+}
