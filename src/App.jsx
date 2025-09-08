6f57e2ae9daabef690d48ee7bb165ca34a70262c
Abdelrhman <adodoadodo2025@gmail.com>
2025-09-09 00:04:34 +0300

    Refactor App component structure to use <main> tag; update styles for body and footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <>
+    <main>
       <Info />
       <About />
       <Interests />
       <Footer />
-    </>
+    </main>
   );
 }
