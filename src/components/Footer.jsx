f227424e87e4d111cdbdf239f3cc9287a011ea09
Abdelrhman <adodoadodo2025@gmail.com>
2025-09-09 00:11:21 +0300

    Wrap social media icons in anchor tags for better accessibility; add hover effects to enhance user interaction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
 export default function Footer() {
   return (
     <footer>
-      <img src="icons/Twitter Icon.svg" alt="twitter icon" />
-      <img src="icons/Facebook Icon.svg" alt="facebook icon" />
-      <img src="icons/Instagram Icon.svg" alt="instagram icon" />
-      <img src="icons/GitHub Icon.svg" alt="github icon" />
+      <a href="">
+        <img src="icons/Twitter Icon.svg" alt="twitter icon" />
+      </a>
+      <a href="">
+        <img src="icons/Facebook Icon.svg" alt="facebook icon" />
+      </a>
+      <a href="">
+        <img src="icons/Instagram Icon.svg" alt="instagram icon" />
+      </a>
+      <a href="">
+        <img src="icons/GitHub Icon.svg" alt="github icon" />
+      </a>
     </footer>
   );
 }
