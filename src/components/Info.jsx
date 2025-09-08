03c36f9e1aad6b1a7bd8e6db26abcd2b5baae00d
Abdelrhman <adodoadodo2025@gmail.com>
2025-09-08 21:41:25 +0300

    Refactor Info component and update styles for consistency

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,14 +1,14 @@
+import DesignImg from "../images/design-img.png";
+
 export default function Info() {
   return (
-    <div>
+    <div className="info">
       <div className="image-container">
-        <img
-          src="/Digital-Buisness-Card/src/images/design-img.png"
-          alt="girl image"
-        />
+        <img src={DesignImg} alt="girl image" />
       </div>
       <h2>Laura Smith</h2>
       <p>Frontend Developer</p>
+      <a href="#">laurasmith.website</a>
     </div>
   );
 }
