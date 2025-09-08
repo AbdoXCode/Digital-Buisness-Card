93e86bff137f065525be8148f4a131b2fee70d9a
Abdelrhman <adodoadodo2025@gmail.com>
2025-09-08 23:54:37 +0300

    Add About, Interests, and Footer components; update App structure and styles
    - Introduced About component with a brief description of the developer.
    - Added Interests component highlighting personal interests.
    - Implemented Footer component with social media icons.
    - Updated App component to render new components.
    - Enhanced CSS styles for sections and improved layout.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <img src="icons/Twitter Icon.svg" alt="twitter icon" />
+      <img src="icons/Facebook Icon.svg" alt="facebook icon" />
+      <img src="icons/Instagram Icon.svg" alt="instagram icon" />
+      <img src="icons/GitHub Icon.svg" alt="github icon" />
+    </footer>
+  );
+}
